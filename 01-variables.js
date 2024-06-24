92c3307a719fb4c0d4300e13d520697122308f27
Luis Najarro <luisnajarrito11@gmail.com>
2024-06-23 22:39:04 -0500

    agregando 1er ejercicio js

---
+++ 01-variables.js
@@ -0,0 +1,12 @@
+let nombre = "Luchito Felizzzz";
+let NombreCompleto = "L Najarro Alcarraz";
+let nombreCompleto = "Luis Alf";
+let nombre_completo = "Li Als";
+
+console.log(NombreCompleto);
+
+let apellido;
+let animal;
+apellido = "Najarro";
+
+console.log(apellido);
